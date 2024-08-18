ac58596dde205b1f952b435345ec18c4fdddd7d7
MR P <apple@MR-Ps-MacBook-Pro.local>
2024-08-18 18:07:50 +0100

    qwerty

--- server.js
+++ server.js
@@ -12,13 +12,13 @@ const port =process.env.PORT || 2002
 
 dbConnect
 
-if(process.env.NODE_ENV === 'production')
-{
-    app.use('/' , express.static('client/build'))
+// if(process.env.NODE_ENV === 'production')
+// {
+//     app.use('/' , express.static('client/build'))
 
-    app.get('*' , (req, res)=>{
-        res.sendFile(path.resolve(__dirname, 'client/build/index.html'))
-    })
-}
+//     app.get('*' , (req, res)=>{
+//         res.sendFile(path.resolve(__dirname, 'client/build/index.html'))
+//     })
+// }
 
 app.listen(port, () => console.log(`Node JS Server started at port ${port}!`))
